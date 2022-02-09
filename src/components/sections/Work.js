1e71c5718dc596fbcda93814e000fdb1112c9ecd
Ezequiel carrizo <ezequielce490@gmail.com>
2022-02-08 21:23:01 -0300

    change the experience section to functional

--- src/components/sections/Work.js
+++ src/components/sections/Work.js
@@ -1,46 +1,36 @@
-import React, { Component } from "react";
+import React from "react";
 import "../../assets/css/App.css";
 import WorkForm from "./WorkForm";
 
-class Work extends Component {
-  constructor(props) {
-    super(props);
+const Work = ({ addForm, deleteForm, workInfo, getInfoWork }) => {
+  const getInfoIndividual = (data, id) => {
+    const workArrayCopy = [...workInfo];
+    const updatedFormIndex = workArrayCopy.findIndex((el) => el.id === id);
+    workArrayCopy[updatedFormIndex].info = data;
+    getInfoWork(workArrayCopy);
+  };
 
-    this.getInfoIndividual = this.getInfoIndividual.bind(this);
-  }
-
-  getInfoIndividual(data, id) {
-    const workArray = [...this.props.workInfo];
-    const updatedFormIndex = workArray.findIndex((el) => el.id === id);
-    workArray[updatedFormIndex].info = data;
-    this.props.getInfoWork(workArray);
-  }
-
-  render() {
-    const { addForm, deleteForm, workInfo } = this.props;
-
-    return (
-      <div className="Work-section">
-        <h2>Work Experience</h2>
-        {workInfo.length < 1 ? (
-          <> </>
-        ) : (
-          workInfo.map((el) => (
-            <WorkForm
-              formInfo={el.info}
-              key={el.id}
-              deleteForm={deleteForm}
-              id={el.id}
-              getInfoIndividual={this.getInfoIndividual}
-            />
-          ))
-        )}
-        <button className="btn-add" onClick={addForm}>
-          add
-        </button>
-      </div>
-    );
-  }
-}
+  return (
+    <div className="Work-section">
+      <h2>Work Experience</h2>
+      {workInfo.length < 1 ? (
+        <> </>
+      ) : (
+        workInfo.map((el) => (
+          <WorkForm
+            formInfo={el.info}
+            key={el.id}
+            deleteForm={deleteForm}
+            id={el.id}
+            getInfoIndividual={getInfoIndividual}
+          />
+        ))
+      )}
+      <button className="btn-add" onClick={addForm}>
+        add
+      </button>
+    </div>
+  );
+};
 
 export default Work;
--- src/components/sections/WorkForm.js
+++ src/components/sections/WorkForm.js
@@ -1,16 +1,9 @@
-import React, { Component } from "react";
+import React from "react";
 import "../../assets/css/App.css";
 
-class WorkForm extends Component {
-  constructor(props) {
-    super(props);
-
-    this.inputHandler = this.inputHandler.bind(this);
-  }
-
-  inputHandler(e) {
+const WorkForm = ({ id, formInfo, getInfoIndividual, deleteForm }) => {
+  const inputHandler = (e) => {
     const inputName = e.target.name;
-    const { id, formInfo, getInfoIndividual } = this.props;
 
     switch (inputName) {
       case "position":
@@ -32,60 +25,56 @@ class WorkForm extends Component {
       default:
         console.log("not found");
     }
-  }
+  };
 
-  render() {
-    const { deleteForm, id } = this.props;
+  const { position, company, city, from, to } = formInfo;
 
-    const { position, company, city, from, to } = this.props.formInfo;
-
-    return (
-      <>
-        <form>
-          <input
-            type="text"
-            placeholder="Position"
-            name="position"
-            onChange={this.inputHandler}
-            value={position}
-          />
-          <input
-            type="text"
-            placeholder="Company"
-            name="company"
-            onChange={this.inputHandler}
-            value={company}
-          />
-          <input
-            type="text"
-            placeholder="City"
-            name="city"
-            onChange={this.inputHandler}
-            value={city}
-          />
-          <input
-            type="text"
-            placeholder="From"
-            name="from"
-            onChange={this.inputHandler}
-            value={from}
-          />
-          <input
-            type="text"
-            placeholder="to"
-            name="to"
-            onChange={this.inputHandler}
-            value={to}
-          />
-          <div className="btn-part">
-            <button className="btn-work" onClick={(e) => deleteForm(e, id)}>
-              delete
-            </button>
-          </div>
-        </form>
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <form>
+        <input
+          type="text"
+          placeholder="Position"
+          name="position"
+          onChange={inputHandler}
+          value={position}
+        />
+        <input
+          type="text"
+          placeholder="Company"
+          name="company"
+          onChange={inputHandler}
+          value={company}
+        />
+        <input
+          type="text"
+          placeholder="City"
+          name="city"
+          onChange={inputHandler}
+          value={city}
+        />
+        <input
+          type="text"
+          placeholder="From"
+          name="from"
+          onChange={inputHandler}
+          value={from}
+        />
+        <input
+          type="text"
+          placeholder="to"
+          name="to"
+          onChange={inputHandler}
+          value={to}
+        />
+        <div className="btn-part">
+          <button className="btn-work" onClick={(e) => deleteForm(e, id)}>
+            delete
+          </button>
+        </div>
+      </form>
+    </>
+  );
+};
 
 export default WorkForm;
